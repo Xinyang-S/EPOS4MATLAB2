921134c933dc6349de2008bd0ff6af8e1357e947
Xinyang Sun <xinyang.sun20@imperial.ac.uk>
2021-06-18 16:04:59 +0100

    upload

--- udp_send_data.m
+++ udp_send_data.m
@@ -2,9 +2,11 @@ clear u1 u2 u3 u4
 u4 = udpport("LocalPort",1000);
 u2 = udpport("LocalPort",3000); % open udp for FES pw from simulink, clear port if error
 force_array = [];
+force_target_array = [];
 encoder_array = [];
 velocity_array = [];
 current_array = [];
+data_string = [];
 
 gripMaintain = {};
 gripForce = {};
@@ -28,69 +30,71 @@ hi5Position = {};
 %% Run Python Code Here
 
 % hi5 initialization
-Motor1 = Epos4(0,0);
-Motor1.ClearErrorState;
-Motor1.DisableNode;
-Motor1.SetOperationMode( OperationModes.CurrentMode );
-Motor1.EnableNode;
-Motor1.ClearErrorState;
+% Motor1 = Epos4(0,0);
+% Motor1.ClearErrorState;
+% Motor1.DisableNode;
+% Motor1.SetOperationMode( OperationModes.CurrentMode );
+% Motor1.EnableNode;
+% Motor1.ClearErrorState;
 
 while(1)
 %target tracking trials 
-velocity = Motor1.ActualVelocity;
-velocity_array = [velocity_array velocity];
-current = Motor1.ActualCurrent;
-current_array = [current_array current];
+% velocity = Motor1.ActualVelocity;
+% velocity_array = [velocity_array velocity];
+% current = Motor1.ActualCurrent;
+% current_array = [current_array current];
 
 %for hi5 tracking traj = sinPos; for grip sensor traj = newton
 %for hi5 position traj = position;
-while (true)
-    flush(u4)
-    read(u4,1,'double')
-end
-
+% flush(u4)
+disp(1)
+app_data = read(u4,4,'string');
+app_data_string = split(app_data,'');
+disp(app_data)
+exp_num = str2num(app_data_string(2))
+trial_num = str2num(app_data_string(3:5))
 
 switch exp_num
     case '1'%hi5 full-assisted target tracking
-        if trail_num ~= 0
-            while trail_num ~= 0
+        if trial_num ~= 0
+            while trial_num ~= 0
                 [exp_num, trial_num, traj] = read(u4,1,'double');
                 hi5_FA_traj = [hi5_FA_traj traj];
                 encoder = Motor1.ActualPosition;
                 encoder_array = [encoder_array encoder];
             end
-        elseif trail_num == 0
-            full_assisted_name = strcat('trail',num2str(trail_num));
+        elseif trial_num == 0
+            full_assisted_name = strcat('trial',num2str(trial_num));
             hi5Target_FA_wristPos.(full_assisted_name) = encoder_array;
             hi5Target_FA_targetPos.(full_assisted_name) = hi5_FA_traj;
             hi5_FA_traj = [];
             encoder_array = [];
         end
     case '2'%hi5 semi-assisted target tracking
-        if trail_num ~= 0
-            while trail_num ~= 0
+        if trial_num ~= 0
+            while trial_num ~= 0
                 [exp_num, trial_num, traj] = read(u4,1,'double');
                 hi5_SA_traj = [hi5_SA_traj traj];
                 encoder = Motor1.ActualPosition;
                 encoder_array = [encoder_array encoder];
             end
-        elseif trail_num == 0
-            semi_assisted_name = strcat('trail',num2str(trail_num));
+        elseif trial_num == 0
+            semi_assisted_name = strcat('trial',num2str(trial_num));
             hi5Target_SA_wristPos.(semi_assisted_name) = encoder_array;
             hi5Target_SA_targetPos.(semi_assisted_name) = hi5_SA_traj;
             hi5_SA_traj = [];
             encoder_array = [];
         end
     case '3'%hi5 zero-assisted target tracking
-        if trail_num ~= 0
-            while trail_num ~= 0
+        if trial_num ~= 0
+            while trial_num ~= 0
                 [exp_num, trial_num, traj] = read(u4,1,'double');
                 hi5_ZA_traj = [hi5_ZA_traj traj];
                 encoder = Motor1.ActualPosition;
                 encoder_array = [encoder_array encoder];
             end
-        elseif trail_num == 0
-            zero_assisted_name = strcat('trail',num2str(trail_num));
+        elseif trial_num == 0
+            zero_assisted_name = strcat('trial',num2str(trial_num));
             hi5Target_ZA_wristPos.(zero_assisted_name) = encoder_array;
             hi5Target_ZA_targetPos.(zero_assisted_name) = hi5_ZA_traj;
             hi5_ZA_traj = [];
@@ -99,18 +103,49 @@ switch exp_num
     case '4'%hi5 position track
         
     case '5'%torque stablization
-    case '6'% grip tracking
-        if trial_num ~= 0
-            force = Getforce();
-            if (~isempty(force))
-                write(u2,[force encoder],"double","LocalHost",4000);
+    case '6'%grip tracking
+        %numTrials = 1; %max number of trials 
+        c = clock;
+        clockStart = c(4)*3600+c(5)*60+c(6);
+        
+        while(exp_num == 6)
+            if trial_num ~= 0
+                c = clock;
+                clockCurrent = c(4)*3600+c(5)*60+c(6);
+                sinTime = clockCurrent - clockStart;
+                force_target = 2*18.51*(sin(sinTime*pi/1.547)*sin(sinTime*pi/2.875));
+                blueY= 6.8*(sinPos)+335;
+                force = Getforce();
+                
+                if (~isempty(force))
+                    force_display = (-10)*((-(60*force))/6400*90)+100;
+                    ErrorSample = sqrt((force_target-force)^2);
+                    Error_array = [Error_array ErrorSample];
+                    Error = mean(Error_array);
+                    data_box = [force_target force_display Error];
+                    for i = 1:(length(data_box))
+                        data_string = [data_string uniform_data(data_box(i))];
+                    end
+                    write(u2,data_string,"string","LocalHost",4000);
+                end
+                force_array = [force_array force];
+                force_target_array = [force_target_array force_target];
+            elseif trial_num == 0
+                force_name = strcat('trial',num2str(trial_num));
+                gripforce.(force_name) = force_array;
+                gripforce_target.(force_name) = force_target_array;
+                force_target_array = [];
+                force_array = [];
             end
-            force_array = [force_array ; force];
-        elseif trial_num == 0
-            force_name = strcat('trial',num2str(trial_num));
-            gripforce.(force_name) = force_array;
-            force_array = [];
+            flush(u4)
+            app_data = read(u4,1,'double');
+            exp_num = app_data(1);
+            trial_num = app_data(2:4); 
         end
+        griptrack.gripforce = gripforce
+        griptrack.gripforcetarget = gripforce_target;
+        save ('griptrack.mat','griptrack');
+        
     case '7'%grip force maintain
     case '9'% save all the data in workspace
         hi5Targer_fullAssisted.WristPos = hi5Target_FA_wristPos;
---
+++ uniform_data_trials.m
@@ -0,0 +1,10 @@
+function uniform_data = uniform_data_trials(data_input)
+    switch length(num2str(data_input))
+        case '1'
+            uniform_data = strcat('0','0',num2str(data_input));
+        case '2'
+            uniform_data = strcat('0',num2str(data_input));
+        case '3'
+            uniform_data = num2str(data_input);
+    end
+end
