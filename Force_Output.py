74871214d0ab70e515c22349494487817c4c52a4
Xinyang Sun <xinyang.sun20@imperial.ac.uk>
2021-06-21 15:07:23 +0100

    update

--- Force_Output.py
+++ Force_Output.py
@@ -3,23 +3,24 @@ import time
 
 godirect = GoDirect(use_ble=False, use_usb=True)
 device = godirect.get_device()
-
-device.open(auto_start=False)
+#output = []
+device.open(auto_start=True)
 device.start(period=10)
 
 sensors = device.get_enabled_sensors()
 i = 1
-while i:
+while i<100:
     device.read()
     print(sensors[0].values[-1])
-    f= open("Force.txt","w+")
+    #output.append(sensors[0].values[-1])
+    '''f= open("Force.txt","w+")
     #if device.read():
         #print(sensors[0].values[-1])
     f.write(str(sensors[0].values[-1]))
         #sensors[0].clear()
-    f.close()
-
-    time.sleep(0.01)
+    f.close()'''
+    i+=1
+    time.sleep(0.001)
 device.stop()
 device.close()
 godirect.quit()
