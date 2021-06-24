19a05b0ebe3383cc905d8f0b02f21f4a207fcff9
Xinyang Sun <xinyang.sun20@imperial.ac.uk>
2021-06-24 10:38:33 +0100

    upload

--- App_udp_Experiment.m
+++ App_udp_Experiment.m
@@ -37,126 +37,6 @@ total_block_num = str2num(cell2mat(strcat(app_data_string(9),app_data_string(10)
 
 switch exp_num
 %     
-% %------------------------Full-assisted target tracking---------------------
-%     case 1 %hi5 full-assisted target tracking
-%         %hi5 initialization
-%         Motor1 = Epos4(0,0);
-%         Motor1.ClearErrorState;
-%         Motor1.DisableNode;
-%         Motor1.SetOperationMode( OperationModes.CurrentMode );
-%         Motor1.EnableNode;
-%         Motor1.ClearErrorState;
-%         
-%         % PID variables
-%         currentMaxP = 10000;
-%         currentMaxN = -10000;
-%         KW = 4*6400/90; %spring: 1 light, 2 med (best demo), 3 large, 4 heavy (limit)
-%         DW = 2*6400/90;
-%         filterLP_D = 0.7; %filter error velocity: 0.7, not very sensitive
-%         posErrorPrev = 0;
-%         posErrorDiff = 0;
-%         
-%         Zero_position = Motor1.ActualPosition;
-%         
-%         hi5Target_fullAssisted = {};%cell aray for positional data
-%         hi5WristPos = {};%cell aray for positional data
-%         hi5TargetPos = {};
-%         hi5Velocity = {};
-%         hi5Current = {};
-%         hi5Error = {};
-%         subject_traj_10_array = zeros(1,10);
-%         elapsed_time_10_array = zeros(1,10);
-%         target_traj_10_array = zeros(1,10);
-%         velocity_10_array = zeros(1,10);
-%         current_10_array = zeros(1,10);
-%         target_traj_array = [];
-%         subject_traj_array = [];
-%         velocity_array = [];
-%         current_array = [];
-%         error_array = [];
-%         
-%         trial_num = 1;
-%         while(trial_num <= total_trial_num)
-%             c = clock;
-%             clockStart = c(4)*3600+c(5)*60+c(6);
-%             clockCurrent = clockStart;
-%             while (clockCurrent < clockStart + trial_length)
-%                 k = 1;
-%                 while k <= 10
-%                     c = clock;
-%                     clockCurrent = c(4)*3600+c(5)*60+c(6);
-%                     % subject position
-%                     subject_current_pos = Motor1.ActualPosition;
-%                     subject_traj = -(subject_current_pos - Zero_position)*90/6400;
-%                     subject_traj_10_array(k) = subject_traj;
-%                     
-%                     % target position
-%                     elapsed_time = clockCurrent - clockStart;
-%                     elapsed_time_10_array(k) = elapsed_time;
-%                     
-%                     target_traj = 2*18.51*(sin(elapsed_time*pi/1.547)*sin(elapsed_time*pi/2.875));
-%                     target_traj_10_array(k) = target_traj;
-%                     
-%                     ErrorSample = sqrt((target_traj-subject_traj)^2);
-%                     Error_array = [Error_array ErrorSample];
-%                     Error = mean(Error_array);
-% 
-%                     %control Hi5 - stiffness
-%                     position_Error = (- subject_traj) - target_traj;
-%                     posErrorDiffNew = position_Error - posErrorPrev;
-%                     posErrorDiff = posErrorDiff*(filterLP_D) + posErrorDiffNew*(1-filterLP_D); %damper can cause small vibrations 
-%                     posErrorPrev = position_Error;
-% 
-%                     current = -1*(KW*position_Error + DW*posErrorDiff);
-% 
-%                     if (current > currentMaxP)
-%                         disp('Wall Too Strong!')
-%                     elseif (current < currentMaxN)
-%                         disp('Wall Broke!')
-%                     else
-%                         current = safetyCheck(current);
-%                         Motor1.MotionWithCurrent(current);
-%                     end
-%                     
-%                     velocity_10_array(k) = Motor1.ActualVelocity;
-%                     current_10_array(k) = Motor1.ActualCurrent;
-%                     k = k+1;
-%                 
-%                 end
-%                 
-%                 data_box = [roundn(target_traj_10_array,-5) roundn(subject_traj_10_array,-5) roundn(Error,-5) roundn(elapsed_time_10_array, -5)];
-% 
-%                 write(u2,data_box,"double","LocalHost",4000);
-% 
-%                 velocity_array = [velocity_array velocity_10_array];
-%                 current_array = [current_array current_10_array];
-%                 target_traj_array = [target_traj_array target_traj_10_array];
-%                 subject_traj_array = [subject_traj_array subject_traj_10_array];
-%             end
-%             Motor1.MotionWithCurrent(0);
-%             trial_name = strcat('trial',num2str(trial_num));
-%             hi5WristPos.(trial_name) = subject_traj_array;
-%             hi5TargetPos.(trial_name) = target_traj_array;
-%             hi5Velocity.(trial_name) = velocity_array;
-%             hi5Current.(trial_name) = current_array;
-%             hi5Error.(trial_name) = Error_array;
-%             target_traj_array = [];
-%             subject_traj_array = [];
-%             velocity_array = [];
-%             current_array = [];
-%             Error_array = [];
-%             disp(trial_num);
-%             trial_num = trial_num+1;
-%             disp('end of trial');
-%             pause(5);% time for ready count down in AppDesigner
-%         end
-%         hi5Target_fullAssisted.hi5WristPos = hi5WristPos;
-%         hi5Target_fullAssisted.hi5TargetPos = hi5TargetPos;
-%         hi5Target_fullAssisted.hi5Velocity = hi5Velocity;
-%         hi5Target_fullAssisted.hi5Current = hi5Current;
-%         hi5Target_fullAssisted.hi5Error = hi5Error;
-%         save ('hi5Target_fullAssisted.mat','hi5Target_fullAssisted');
-
 %------------------------Full-assisted target tracking---------------------
     case 1 %hi5 full-assisted target tracking
         
@@ -768,7 +648,7 @@ switch exp_num
         fopen(u2);
         total_trial_num = 2;
         trial_length = 10;
-        disp('Task: grip tracking')
+        disp('Task 6: grip tracking')
         force_array = [];
         force_target_array = [];
         Error_array = [];
@@ -786,45 +666,67 @@ switch exp_num
             clockCurrent = clockStart; 
             Error_array = [];
             Error = 0;
-            while (clockCurrent < clockStart + trial_length)
-                
-                k = 1;
-                while k <= 10
+            while (clockCurrent < clockStart + trial_length + 5)
+                if clockCurrent < clockStart + 5 % countdown
+                    k = 1;
+                    while k <= 10
+                        c = clock;
+                        clockCurrent = c(4)*3600+c(5)*60+c(6);
+                        elapsed_time(k) = clockCurrent - clockStart;
+                        k = k+1;
+                    end
+                    
+                    force_target = -[37.02 37.02 37.02 37.02 37.02 37.02 37.02 37.02 37.02 37.02];
+                    force = read(u_force,10,'single');
+                    data_box = [roundn(force_target,-5) roundn(force,-5) roundn(Error,-5) roundn(elapsed_time, -5)];
+                    elapsed_time = [];
+
+                    newV = typecast(single(data_box), 'int8')
+                    fwrite(u2, newV, 'int8')
+                    traj_start_flag = 1;
+                elseif clockCurrent >= clockStart + 5 % in trial
+                    
                     c = clock;
                     clockCurrent = c(4)*3600+c(5)*60+c(6);
-                    elapsed_time(k) = clockCurrent - clockStart;
-                    k = k+1;
+                    k = 1;
+                    
+                    if traj_start_flag == 1
+                        c = clock;
+                        traj_start_time = c(4)*3600+c(5)*60+c(6);
+                        traj_start_flag = ~traj_start_flag;
+                    end
+                    while k <= 10
+                        c = clock;
+                        clockCurrent = c(4)*3600+c(5)*60+c(6);
+                        elapsed_time(k) = clockCurrent - clockStart;
+                        k = k+1;
+                    end
+                    force_target =2.*18.51.*(sin((elapsed_time-traj_start_time+10).*pi/1.547).*sin((elapsed_time-traj_start_time+10).*pi/2.875));
+                    force = read(u_force,10,'single');
+    %                 ErrorSample = sqrt((force_target-force).^2);
+    %                 Error_array = [Error_array ErrorSample];
+    %                 Error = mean(Error_array);
+                    data_box = [roundn(force_target,-5) roundn(force,-5) roundn(Error,-5) roundn(elapsed_time, -5)];
+                    elapsed_time = [];
+
+                    newV = typecast(single(data_box), 'int8')
+                    fwrite(u2, newV, 'int8')
+                    force_array = [force_array force];
+                    force_target_array = [force_target_array force_target];
+                    force_target = [];
                 end
-                force_target =2.*18.51.*(sin(elapsed_time.*pi/1.547).*sin(elapsed_time.*pi/2.875));
-%                 flush(u_force)
-                force = read(u_force,10,'single');
-%                 ErrorSample = sqrt((force_target-force).^2);
-%                 Error_array = [Error_array ErrorSample];
-%                 Error = mean(Error_array);
-                data_box = [roundn(force_target,-5) roundn(force,-5) roundn(Error,-5) roundn(elapsed_time, -5)];
-%                 disp(data_box);
-                elapsed_time = [];
-                
-                newV = typecast(single(data_box), 'int8')
-                fwrite(u2, newV, 'int8')
-%                 write(u2,data_box,"double","LocalHost",4000);
-                force_array = [force_array force];
-                force_target_array = [force_target_array force_target];
-                force_target = [];
-                
             end
             
             force_name = strcat('trial',num2str(trial_num));
             gripforce.(force_name) = force_array;
             gripforce_target.(force_name) = force_target_array;
-            gripforce_error.(force_name) = Error_array;
+%             gripforce_error.(force_name) = Error_array;
             force_target_array = [];
             force_array = [];
             Error_array = [];
             disp(trial_num);
             trial_num = trial_num+1;
             disp('end of trial');
-            pause(5);% time for ready count down in AppDesigner
         end
         gripTrack.gripForce = gripforce;
         gripTrack.gripForceTarget = gripforce_target;
@@ -836,7 +738,7 @@ switch exp_num
     case 7 %grip force maintain
         fclose(u2)
         fopen(u2);
-        disp('Task: grip force maintain')
+        disp('Task 7: grip force maintain')
         force_array = [];
         force_target_array = [];
         block_num = 1;
